9518498d5ba18dc8fcdda96d1beb685d6ba1bebb
Adeoti Adegboyega <adeotiadegboyega@gmail.com>
2020-06-03 13:50:00 +0100

    created createvote view

--- django/core/views.py
+++ django/core/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from django.views.generic import (ListView, DetailView)
+from django.shortcuts import render, redirect
+from django.views.generic import (ListView, DetailView, CreateView)
 from core.models import Movie, Person, Vote
 from django.urls import reverse
-from forms import VoteForm
+from core.forms import VoteForm
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class MovieList(ListView):
 
@@ -37,3 +39,31 @@ class MovieDetail(DetailView):
 class PersonDetail(DetailView):
 
     queryset = Person.objects.all_with_prefetch_movies()
+
+class CreateVote(LoginRequiredMixin, CreateView):
+    form_class = VoteForm
+
+    def get_initial(self):
+
+        initial = super.get_initial()
+        initial['user'] = self.request.user.id
+        initial['movie'] = self.kwargs['movie_id']
+
+        return initial
+    
+    def get_success_url(self):
+        movie_id = self.object.movie.id
+
+        return reverse(
+                    'core:MovieDetail',
+                    kwargs={
+                        'pk': movie_id})
+    
+    def render_to_response(self, context, **response_kwargs):
+        movie_id = context['object'].id
+
+        movie_detail_url = reverse(
+                    'core:MovieDetail',
+                    kwargs={
+                        'pk': movie_id})
+        return redirect(to=movie_detail_url)
